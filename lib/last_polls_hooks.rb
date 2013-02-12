823dcf83c66d41c63ebdc9699942fe288a366e47
rubynovich <roman@shipiev.me>
2013-02-12 11:05:08 +0400

    fix

--- lib/last_polls_hooks.rb
+++ lib/last_polls_hooks.rb
@@ -1,9 +1,9 @@
 class LastPollsHooks < Redmine::Hook::ViewListener
 
   def view_welcome_index_right(context = { })
-    if User.current.projects.any? && 
+    if User.current.projects.any? &&
        User.current.logged? &&
-       !Object.const_defined?('HomeBlock') || HomeBlock.find_by_name('polls')
+       (!Object.const_defined?('HomeBlock') || HomeBlock.find_by_name('polls'))
 
       if context[:hook_caller].respond_to?(:render)
         context[:hook_caller].send(:render, {:locals => {:polls => VotingPoll.unvoted_for_all()}.merge(context), :partial => '/polls/polls_box'})
@@ -16,16 +16,16 @@ class LastPollsHooks < Redmine::Hook::ViewListener
   def view_projects_show_right(context = { })
     project = context[:project]
 
-    if User.current.logged? && 
+    if User.current.logged? &&
        (!Object.const_defined?('OverviewBlock') || OverviewBlock.find_by_project_id_and_name(project, 'polls'))
 
       if context[:hook_caller].respond_to?(:render)
-        context[:hook_caller].send(:render, {:locals => {:polls => VotingPoll.unvoted(project.id)}.merge(context), :partial => '/polls/polls_box'}) 
+        context[:hook_caller].send(:render, {:locals => {:polls => VotingPoll.unvoted(project.id)}.merge(context), :partial => '/polls/polls_box'})
       elsif context[:controller].is_a?(ActionController::Base)
-        context[:controller].send(:render_to_string, {:locals => {:polls => VotingPoll.unvoted(project.id)}.merge(context), :partial => '/polls/polls_box'}) 
+        context[:controller].send(:render_to_string, {:locals => {:polls => VotingPoll.unvoted(project.id)}.merge(context), :partial => '/polls/polls_box'})
       end
     end
 
-    
+
   end
 end
