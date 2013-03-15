b4b59afb4978bee0f7e634c86ef12ef1949c8e15
rubynovich <roman@shipiev.me>
2013-03-15 13:28:58 +0400

    ref: remove extra code

--- lib/last_polls_hooks.rb
+++ lib/last_polls_hooks.rb
@@ -1,10 +1,7 @@
 class LastPollsHooks < Redmine::Hook::ViewListener
 
   def view_welcome_index_right(context = { })
-    if User.current.projects.any? &&
-       User.current.logged? &&
-       (!Object.const_defined?('HomeBlock') || HomeBlock.find_by_name('polls'))
-
+    if User.current.projects.any? && User.current.logged?
       if context[:hook_caller].respond_to?(:render)
         context[:hook_caller].send(:render, {:locals => {:polls => VotingPoll.unvoted_for_all()}.merge(context), :partial => '/polls/polls_box'})
       elsif context[:controller].is_a?(ActionController::Base)
@@ -16,9 +13,7 @@ class LastPollsHooks < Redmine::Hook::ViewListener
   def view_projects_show_right(context = { })
     project = context[:project]
 
-    if User.current.logged? &&
-       (!Object.const_defined?('OverviewBlock') || OverviewBlock.find_by_project_id_and_name(project, 'polls'))
-
+    if User.current.logged?
       if context[:hook_caller].respond_to?(:render)
         context[:hook_caller].send(:render, {:locals => {:polls => VotingPoll.unvoted(project.id)}.merge(context), :partial => '/polls/polls_box'})
       elsif context[:controller].is_a?(ActionController::Base)
